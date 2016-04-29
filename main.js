70d768a30800942e379839ccf413ef2445fd973a
Corey Birnbaum <vonwolfehaus@gmail.com>
2016-04-28 18:59:26 -0700

    update

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-var example = '#### An h4 header\n\nParagraphs are separated by two newlines.\n\n2nd paragraph. _Italic_, **bold**, **or _both_**. Itemized lists look like:\n * this one\n * that one\n * the other one\n\n---\nAfter that separator, numbered lists like this:\n1. first\n  * first sub-item (bullet)\n2. second etc..\n  1. 2nd sub-item (numeric)';
+var example = '#### An h4 header\n\nParagraphs are separated by two newlines.\n\n2nd paragraph. _Italic_, **bold**, **or _both_**. Itemized lists look like:\n * this one\n * that one\n * the other one\n---\nAfter that separator, numbered lists like this:\n1. first\n  * first sub-item (bullet)\n2. second etc..\n  1. 2nd sub-item (numeric)';
 
 $(function() {
 	// init
-	$('#markdown').text(example);
+	marked.setOptions({
+		gfm: true,
+		breaks: true
+	});
+	$('#markdown').val(example);
 	$('#output').html(marked(example));
 
 	// key event
 	$('#markdown').bind('keyup', function() {
-		input = $('#markdown').text();
+		input = $('#markdown').val();
 		$('#output').html(marked(input));
 	});
 
@@ -15,7 +19,7 @@ $(function() {
 	$('#clearButton').click(function(event) {
 		event.preventDefault();
 		if (window.confirm('Are you sure you want to delete?')) {
-			$('#markdown').text('');
+			$('#markdown').val('');
 			$('#output').html('');
 		}
 	});
